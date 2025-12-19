e45c7c1df06b02425d39993d87ba6b1989ab16d3
Scott Forslund <scottforslund982@gmail.com>
2025-12-18 17:16:09 -0700

    Add Tailwind CDN to _document.js

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Tailwind CSS CDN - This will make styling work immediately */}
         <script src="https://cdn.tailwindcss.com"></script>
       </Head>
       <body>
